505336ed42474adcf45bd787e694a0f6f86e73bf
Yingzhong <dyinnz@gmail.com>
2018-12-01 11:47:47 +0800

    update sample log_entry

--- log_entry/log_entry.py
+++ log_entry/log_entry.py
@@ -5,11 +5,15 @@
 
 SOURCE_TIDB = 0
 SOURCE_TIKV = 1
-SOURCE_METRIC = 2
-SOURCE_OS = 3
+SOURCE_PD = 2
 
-sample_tidb_log_entry = {
-    'source': SOURCE_TIDB,
+SOURCE_METRIC = 10
+SOURCE_OS = 11
+
+# 因为map结构没有固定schema，我们可以针对一种log类型做任意扩展
+
+sample_tikv_log_entry = {
+    'source': SOURCE_TIKV,
     'log_time': '2018/11/30 13:24:05.143',
     'log_level': 'INFO',
     'file_name': 'tikv-server.rs',
@@ -17,6 +21,27 @@ sample_tidb_log_entry = {
     'content': '', # 剩下的全部内容
     'region': 11, # optional
     'store': 1, # optional
+}
+
+sample_tidb_log_entry = {
+    'source': SOURCE_TIDB,
+    'log_time': '2018/11/30 13:24:08.601',
+    'log_level': 'info',
+    'file_name': 'client.go',
+    'file_line': 398,
+    'content': '', # 剩下的全部内容
+    'tag': ['SLOW_QUERY', 'INTERNAL'] # optional
+}
+
+sample_pd_log_entry = {
+    'source': SOURCE_PD
+    'log_time': '2018/11/30 13:24:08.601',
+    'log_level': 'info',
+    'file_name': 'cluster_info.go',
+    'file_line': 398,
+    'content': '', # 剩下的全部内容
+    'region': 12, # optional
+    'tag': ['SLOW_QUERY', 'INTERNAL'] # optional
 }
 
 # 如何抽取一部分 log, 大概的接口，具体实现再议
